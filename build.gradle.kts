d1cc0ec21b1772cc221bde3481e7ccc2ddce477f
Qing Chen <qing.chen@thoughtworks.com>
2020-06-08 15:39:02 +0800

    [#40][qing & shiji]: modify ci

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ tasks.test {
 }
 
 tasks.register<Copy>("initGitHooks") {
-    from(".git/hooks/")
+    from("./pre-push")
     into("./.git/hooks")
     // set file mode as 755
     fileMode = 0b000_111_101_101
