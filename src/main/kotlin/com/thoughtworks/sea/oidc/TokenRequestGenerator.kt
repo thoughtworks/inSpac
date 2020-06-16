3db9dbb3f927c8c4a1f5bab159de2549d2ed412c
Shuchen Zhang <shuchen.zhang@thoughtworks.com>
2020-06-16 18:16:13 +0800

    Revert "[#45][Youyou & Qirong]: extract default constants; Generate token request."

--- src/main/kotlin/com/thoughtworks/sea/oidc/TokenRequestGenerator.kt
+++
@@ -1,9 +0,0 @@
-package com.thoughtworks.sea.oidc
-
-class TokenRequestGenerator {
-    fun generate(tokenTokenRequestParams: TokenRequestParams): TokenRequestEntity = TokenRequestEntity(
-        tokenTokenRequestParams.host + tokenTokenRequestParams.endPoint,
-        TokenRequestHeader(),
-        TokenRequestBody(tokenTokenRequestParams.code, tokenTokenRequestParams.clientId, tokenTokenRequestParams.clientSecret, tokenTokenRequestParams.redirectUri)
-    )
-}
