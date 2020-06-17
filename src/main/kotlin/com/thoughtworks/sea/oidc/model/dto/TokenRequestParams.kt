a4a49aee101cedc957eaa700844a57a3c4fe3d2a
QirongZhang <qrzhang@thoughtworks.com>
2020-06-17 11:28:30 +0800

    [#45][Pengfei & Qirong]: delete random UUID for request code.

--- src/main/kotlin/com/thoughtworks/sea/oidc/model/dto/TokenRequestParams.kt
+++ src/main/kotlin/com/thoughtworks/sea/oidc/model/dto/TokenRequestParams.kt
@@ -1,14 +1,12 @@
 package com.thoughtworks.sea.oidc.model.dto
 
-import java.util.UUID
-
 data class TokenRequestParams(
     val host: String,
     val endPoint: String,
     val clientId: String,
     val clientSecret: String,
     val redirectUri: String,
-    val code: String = UUID.randomUUID().toString(),
+    val code: String,
     val grantType: String = "authorization_code",
     val contentType: String = "application/x-www-form-urlencoded"
 )
