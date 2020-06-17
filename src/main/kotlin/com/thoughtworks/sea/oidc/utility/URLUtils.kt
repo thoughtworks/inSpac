84edacd559f1ee78a8688fcf9a37b561df033a91
Shuchen Zhang <shuchen.zhang@thoughtworks.com>
2020-06-17 14:29:23 +0800

    [#44][Shuchen & Jiajun & Xiaomeng]: Mark generateLoginURL method a static method for better being called / referred in other projects.

--- src/main/kotlin/com/thoughtworks/sea/oidc/utility/URLUtils.kt
+++ src/main/kotlin/com/thoughtworks/sea/oidc/utility/URLUtils.kt
@@ -3,13 +3,16 @@ package com.thoughtworks.sea.oidc.utility
 import com.thoughtworks.sea.oidc.model.dto.SPCPLoginRequest
 
 class URLUtils {
-    fun generateLoginURL(spcpLoginRequest: SPCPLoginRequest): String =
-        "https://${spcpLoginRequest.host}" +
-                "${spcpLoginRequest.endPoint}?" +
-                "scope=${spcpLoginRequest.scope}&" +
-                "response_type=${spcpLoginRequest.responseType}&" +
-                "client_id=${spcpLoginRequest.clientId}&" +
-                "nonce=${spcpLoginRequest.nonce}&" +
-                "state=${spcpLoginRequest.state}&" +
-                "redirect_uri=${spcpLoginRequest.redirectURI}"
+    companion object {
+        @JvmStatic
+        fun generateLoginURL(spcpLoginRequest: SPCPLoginRequest): String =
+            "https://${spcpLoginRequest.host}" +
+                    "${spcpLoginRequest.endPoint}?" +
+                    "scope=${spcpLoginRequest.scope}&" +
+                    "response_type=${spcpLoginRequest.responseType}&" +
+                    "client_id=${spcpLoginRequest.clientId}&" +
+                    "nonce=${spcpLoginRequest.nonce}&" +
+                    "state=${spcpLoginRequest.state}&" +
+                    "redirect_uri=${spcpLoginRequest.redirectURI}"
+    }
 }
