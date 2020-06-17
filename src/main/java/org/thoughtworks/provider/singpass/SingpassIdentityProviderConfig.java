901d6b27d369ab11ad81719ceb2ce4d9e8f5dd77
yuexie.zhou <yuexie.zhou@thoughtworks.com>
2020-06-17 17:24:35 +0800

    [#95][Yuexie & Fuyi]: remove not need log in singpass config

--- src/main/java/org/thoughtworks/provider/singpass/SingpassIdentityProviderConfig.java
+++ src/main/java/org/thoughtworks/provider/singpass/SingpassIdentityProviderConfig.java
@@ -8,22 +8,15 @@ public class SingpassIdentityProviderConfig extends OIDCIdentityProviderConfig {
 
   public SingpassIdentityProviderConfig(IdentityProviderModel model) {
     super(model);
-
-    System.out.println("SingpassIdentityProviderConfig: model");
   }
 
   public SingpassIdentityProviderConfig() {}
 
   public void setPrivateKey(String privateKey) {
-    System.out.println("SingpassIdentityProviderConfig: set value");
-    System.out.println(privateKey);
-
     getConfig().put("privateKey", privateKey);
   }
 
   public String getPrivateKey() {
-    System.out.println("SingpassIdentityProviderConfig: get value");
-
     return getConfig().get("privateKey");
   }
 }
