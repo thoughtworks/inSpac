2724d56f4401ec30c92fabd942088f22dad38101
Shuchen Zhang <shuchen.zhang@thoughtworks.com>
2020-06-17 15:06:00 +0800

    [#44][Shuchen & Xiaomeng]: Add unit test for generateInitAuthURL method of URLUtils.

---
+++ src/test/kotlin/com/thoughtworks/sea/oidc/utility/URLUtilsTest.kt
@@ -0,0 +1,27 @@
+package com.thoughtworks.sea.oidc.utility
+
+import com.thoughtworks.sea.oidc.model.dto.InitAuthRequest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class URLUtilsTest {
+
+    @Test
+    internal fun `should generate SingPass Login URL when calling generateLoginAddress method`() {
+
+        val initAuthRequest = InitAuthRequest(
+            "localhost",
+            "/auth2.0",
+            "SDK-TEST-001",
+            "https://google.com",
+            nonce = "UUID-NONCE",
+            state = "UUID-STATE"
+        )
+
+        val actualURL = URLUtils.generateInitAuthURL(initAuthRequest)
+        val expectedURL =
+            "https://localhost/auth2.0?scope=openid&response_type=code&client_id=SDK-TEST-001&nonce=UUID-NONCE&state=UUID-STATE&redirect_uri=https://google.com"
+
+        assertEquals(actualURL, expectedURL)
+    }
+}
