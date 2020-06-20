117439a6ae35c67fec20a1528582d34ac305b20a
yuexie.zhou <yuexie.zhou@thoughtworks.com>
2020-06-20 08:51:58 +0800

    [#95][Yuexie & Fuyi]: Remove no need custom method

--- src/main/java/com/thoughtworks/provider/singpass/SingpassIdentityProvider.java
+++ src/main/java/com/thoughtworks/provider/singpass/SingpassIdentityProvider.java
@@ -7,69 +7,18 @@ import com.thoughtworks.provider.singpass.utils.PrivateKeyUtils;
 import java.io.IOException;
 import java.security.PrivateKey;
 import java.text.ParseException;
-import org.jboss.logging.Logger;
 import org.keycloak.broker.oidc.OIDCIdentityProvider;
-import org.keycloak.broker.provider.BrokeredIdentityContext;
 import org.keycloak.broker.provider.IdentityBrokerException;
-import org.keycloak.common.util.Time;
 import org.keycloak.jose.jws.JWSInput;
 import org.keycloak.jose.jws.JWSInputException;
 import org.keycloak.models.KeycloakSession;
-import org.keycloak.protocol.oidc.OIDCLoginProtocol;
-import org.keycloak.representations.AccessTokenResponse;
 import org.keycloak.representations.JsonWebToken;
-import org.keycloak.util.JsonSerialization;
 
 /** @author yuexie.zhou */
 public class SingpassIdentityProvider extends OIDCIdentityProvider {
 
-  // Maybe changed by OIDCIdentityProvider
-  public static final String BROKER_NONCE_PARAM = "BROKER_NONCE";
-  private static final Logger log = Logger.getLogger(SingpassIdentityProvider.class);
-
   public SingpassIdentityProvider(KeycloakSession session, SingpassIdentityProviderConfig config) {
     super(session, config);
-  }
-
-  @Override
-  public BrokeredIdentityContext getFederatedIdentity(String response) {
-    AccessTokenResponse tokenResponse;
-    try {
-      tokenResponse = JsonSerialization.readValue(response, AccessTokenResponse.class);
-    } catch (IOException e) {
-      throw new IdentityBrokerException("Could not decode access token responses.", e);
-    }
-    String accessToken = verifyAccessToken(tokenResponse);
-
-    String encodedIdToken = tokenResponse.getIdToken();
-
-    JsonWebToken idToken = validateToken(encodedIdToken);
-
-    try {
-      BrokeredIdentityContext identity = extractIdentity(tokenResponse, accessToken, idToken);
-
-      if (!identity.getId().equals(idToken.getSubject())) {
-        throw new IdentityBrokerException(
-            "Mismatch between the subject in the id_token and the subject from the user_info endpoint");
-      }
-
-      identity
-          .getContextData()
-          .put(BROKER_NONCE_PARAM, idToken.getOtherClaims().get(OIDCLoginProtocol.NONCE_PARAM));
-
-      if (getConfig().isStoreToken()) {
-        if (tokenResponse.getExpiresIn() > 0) {
-          long accessTokenExpiration = Time.currentTime() + tokenResponse.getExpiresIn();
-          tokenResponse.getOtherClaims().put(ACCESS_TOKEN_EXPIRATION, accessTokenExpiration);
-          response = JsonSerialization.writeValueAsString(tokenResponse);
-        }
-        identity.setToken(response);
-      }
-
-      return identity;
-    } catch (Exception e) {
-      throw new IdentityBrokerException("Could not fetch attributes from userinfo endpoint.", e);
-    }
   }
 
   @Override
@@ -80,14 +29,6 @@ public class SingpassIdentityProvider extends OIDCIdentityProvider {
 
     JsonWebToken token;
     try {
-      // convert string to object
-      //      ObjectMapper mapper = new ObjectMapper();
-      //      mapper.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
-      //      SingpassJWEResponse jweResponse = mapper.readValue(encodedIdToken,
-      // SingpassJWEResponse.class);
-
-      // https://connect2id.com/products/nimbus-jose-jwt/examples/jwe-with-preset-cek
-      logger.info(encodedIdToken);
       JWEObject jweObject = JWEObject.parse(encodedIdToken);
       PrivateKey privateKey = PrivateKeyUtils.parsePrivateKey(getSingpassConfig().getPrivateKey());
       jweObject.decrypt(new RSADecrypter(privateKey));
@@ -133,15 +74,6 @@ public class SingpassIdentityProvider extends OIDCIdentityProvider {
     }
 
     return token;
-  }
-
-  private String verifyAccessToken(AccessTokenResponse tokenResponse) {
-    String accessToken = tokenResponse.getToken();
-    if (accessToken == null) {
-      throw new IdentityBrokerException("No access_token from server.");
-    } else {
-      return accessToken;
-    }
   }
 
   private SingpassIdentityProviderConfig getSingpassConfig() {
