04a06a642b757324b9050b259bb93e50e2f03118
yuexie.zhou <yuexie.zhou@thoughtworks.com>
2020-06-23 18:11:39 +0800

    [#128][Yuexie & Fuyi]: fixed before test

--- src/test/java/com/thoughtworks/provider/singpass/SingpassIdentityProviderTest.java
+++ src/test/java/com/thoughtworks/provider/singpass/SingpassIdentityProviderTest.java
@@ -4,7 +4,10 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import com.thoughtworks.provider.singpass.SingpassIdentityProviderConfig.LinkToType;
+import java.io.IOException;
 import java.util.Collections;
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -22,55 +25,10 @@ public class SingpassIdentityProviderTest {
   @Mock() private SingpassIdentityProviderConfig config;
 
   @BeforeAll
-  public void setUp() {
+  public void setUp() throws IOException {
     MockitoAnnotations.initMocks(this);
 
-    IdentityProviderModel model = new IdentityProviderModel();
-    config = new SingpassIdentityProviderConfig(model);
-    config.setDefaultScope("openid");
-    config.setClientId("client_id");
-    config.setValidateSignature(false);
-    config.setPublicKeySignatureVerifier(
-        "-----BEGIN PUBLIC KEY-----\n"
-            + "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAu3YvExh1qjJBpkjALKfC\n"
-            + "uB67KqjzCc/EwUrBOkdoEYNLur80A2Y3R4v2q1AW8f8e3bMiPLLSlvxS0a0YyaiT\n"
-            + "2+4/ENjwy+dUa1o9XdiWLmHWWMWi7je1IK4zsDxoIt9nSOxaZmhu4aZeP+GhrMiV\n"
-            + "LnV0xKDl/4n2QZbD1GkdkG4ZuyqHJsmGcGtXLU1U6r6p6ZDrCTkBXjX96EDobHfX\n"
-            + "VFfi/RJ7VOD2aP2p4G7I104tNBYmB9+mkHr4AuOT9PhmOqq4a9Fo1E6VLnlo4FW/\n"
-            + "2ZK10gUU0f5wacnNillER0NGNwFWoTj2ASF9LiR6/39b4satRLAy8RxMclva8iK6\n"
-            + "yQIDAQAB\n"
-            + "-----END PUBLIC KEY-----\n");
-    config.setPrivateKey(
-        "-----BEGIN PRIVATE KEY-----\n"
-            + "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC7di8TGHWqMkGm\n"
-            + "SMAsp8K4HrsqqPMJz8TBSsE6R2gRg0u6vzQDZjdHi/arUBbx/x7dsyI8stKW/FLR\n"
-            + "rRjJqJPb7j8Q2PDL51RrWj1d2JYuYdZYxaLuN7UgrjOwPGgi32dI7FpmaG7hpl4/\n"
-            + "4aGsyJUudXTEoOX/ifZBlsPUaR2Qbhm7KocmyYZwa1ctTVTqvqnpkOsJOQFeNf3o\n"
-            + "QOhsd9dUV+L9EntU4PZo/angbsjXTi00FiYH36aQevgC45P0+GY6qrhr0WjUTpUu\n"
-            + "eWjgVb/ZkrXSBRTR/nBpyc2KWURHQ0Y3AVahOPYBIX0uJHr/f1vixq1EsDLxHExy\n"
-            + "W9ryIrrJAgMBAAECggEAY8jsE+kQMRFhWqcdDGgcQS+yh2m5PP7Ih+9H3cLGxZOz\n"
-            + "CuvePvT49e+t1NDj9drMTkydK9wwNsiHOS8/o5BFbGtsTIZ93rv7ds1pHvw8LOJN\n"
-            + "W6GQMeebVZME1onBENcEPo/5KsvqQdjyEGUFT1jR+BHznvrakuSYHZ+oC/gMEaVw\n"
-            + "dUsM1849EbLDJ5lOPDDqYwsJwIGEryxRLFP+4HhGR9wnrTVee5CCsH0ep8OqypvY\n"
-            + "xgg9Ytyt1WripwzhXsVzJxahTbO4XImOgv6Uvo3EYfBXm1gbfugGbSiyYHBBx0Pa\n"
-            + "7DtYrpRzjeS33m6Y4SJjKERjCbHXChwUrFcMGS5ogQKBgQDeChbIHYRu4HTQYoV9\n"
-            + "pJ70LcHCiE0zaJzuMHes4OFkNAuFXASDGp1HJXd0tol+oeuYO/q1gENvyfBAqDH/\n"
-            + "AVAtWaFvQUNzv/Zs++mbrSSwxuZJCaJAbh9GKzCyCbapxBtdzhgCLLY9GeWRiG1c\n"
-            + "puYUUvJmkcWQE9sSn2tnE8mv2QKBgQDYIjWgagTLPlT3J65t5BEs9fwjAq8bdIIV\n"
-            + "E9o0blIwlKF3FzeUo4Egjc/vfIYOL43AOttuVbVoruvvT13a3+VpCXjxqSYnveuw\n"
-            + "FB0xQC1F0c3ay4Oprjh2qk2GfOy0gikaRgmTUP3nVK1LC57GWJMker/UQ2kWCJCs\n"
-            + "RyMzpgd8cQKBgQCE5q8KKrjJEOp6jG3wbWeDKhwuzxy+Z6B+5V3MiXH/YzN+KDy/\n"
-            + "KF/5ZNCieFvGAy8cGNKQbuxubgWy/bmnM+cErgB1si+oib77LrF+L92lPfg6wVxv\n"
-            + "ijqH6nQkLLI73RiwRhqSuqZ93hFN0cX7zh4rDhbvE9OX0HqxI+DKesqeyQKBgEln\n"
-            + "hPMQTsSATPcMAQ/Nb4/nk1SIqtQWQ7/I2EkKVtus/xGlTvkqdsaJo19g2V6kA+6P\n"
-            + "jsrwTQZaskK6n9OgSxfbYbohipXgyNUqX6fEdhvKX7G5gOP2CbMzr9THRNUhh7gm\n"
-            + "pUXlMfaJKbndHnWay46OKex7YItdKVV5a5k1AEHhAoGBAIVSqdn2f4mVgdQxDls6\n"
-            + "DPp0XJW2D9nI9w9mm+wZc9TtI7X+7+1LZ609vDHAEfj8flTMl0t/ovaaZWmEHCpg\n"
-            + "FKM9rIqep3tZy6qqgo63peDk+RPV3UVgnUwq0+mBwhnHPWQf2nfYnZZHwsldfHWP\n"
-            + "y0U6Qc7UNI3EjJEJPTI4Zifg\n"
-            + "-----END PRIVATE KEY-----");
-
-    //    when(session.getContext().getRealm()).thenReturn()
+    setSingpassConfig();
   }
 
   @Test
@@ -103,9 +61,9 @@ public class SingpassIdentityProviderTest {
         singpassIdentityProvider.extractIdentity(null, null, jsonWebToken);
 
     assertEquals(subject, identity.getId());
-    assertEquals("none", identity.getFirstName());
-    assertEquals(uuid, identity.getLastName());
-    assertEquals("none", identity.getUsername());
+    assertEquals("NONE", identity.getFirstName());
+    assertEquals(subject, identity.getLastName());
+    assertEquals(uuid, identity.getUsername());
     assertEquals(uuid + SingpassIdentityProvider.EMAIL_HOST, identity.getEmail());
   }
 
@@ -124,9 +82,9 @@ public class SingpassIdentityProviderTest {
 
     assertEquals(subject, identity.getId());
     assertEquals(nric, identity.getFirstName());
-    assertEquals(uuid, identity.getLastName());
-    assertEquals(nric, identity.getUsername());
-    assertEquals(nric + SingpassIdentityProvider.EMAIL_HOST, identity.getEmail());
+    assertEquals(subject, identity.getLastName());
+    assertEquals(uuid, identity.getUsername());
+    assertEquals(uuid + SingpassIdentityProvider.EMAIL_HOST, identity.getEmail());
   }
 
   @Test
@@ -145,8 +103,22 @@ public class SingpassIdentityProviderTest {
 
     assertEquals(subject, identity.getId());
     assertEquals(nric, identity.getFirstName());
-    assertEquals(uuid, identity.getLastName());
-    assertEquals(nric, identity.getUsername());
-    assertEquals(nric + SingpassIdentityProvider.EMAIL_HOST, identity.getEmail());
+    assertEquals(subject, identity.getLastName());
+    assertEquals(uuid, identity.getUsername());
+    assertEquals(uuid + SingpassIdentityProvider.EMAIL_HOST, identity.getEmail());
+  }
+
+  private void setSingpassConfig() throws IOException {
+    IdentityProviderModel model = new IdentityProviderModel();
+    config = new SingpassIdentityProviderConfig(model);
+    config.setDefaultScope("openid");
+    config.setClientId("client_id");
+    config.setValidateSignature(false);
+    config.setPrivateKey(
+        IOUtils.toString(this.getClass().getResourceAsStream("/keycloakPrivateKey.pem"), "UTF-8"));
+    config.getConfig().put("firstNameLinkToType", LinkToType.NRIC.name());
+    config.getConfig().put("userNameLinkToType", LinkToType.UUID.name());
+    config.getConfig().put("lastNameLinkToType", LinkToType.SUB.name());
+    config.getConfig().put("autoRegister", "true");
   }
 }
