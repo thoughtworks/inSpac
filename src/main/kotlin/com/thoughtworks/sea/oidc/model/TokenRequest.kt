8d426dca301fbe45ce9f14f7da53b8a78057b392
Jiajun Liu <jiajun.liu@thoughtworks.com>
2020-06-24 15:24:03 +0800

    [#46][Xiaomeng & Jiajun]: extract TokenResponse from TokenRequestAndResponse.kt

---
+++ src/main/kotlin/com/thoughtworks/sea/oidc/model/TokenRequest.kt
@@ -0,0 +1,12 @@
+package com.thoughtworks.sea.oidc.model
+
+import org.springframework.http.HttpEntity
+import org.springframework.util.LinkedMultiValueMap
+
+/** Those are params for getting token by using restTemplate. */
+data class TokenRequest(
+    /** Getting token url. */
+    val url: String,
+    /** HttpEntity contains request body and header. */
+    val httpEntity: HttpEntity<LinkedMultiValueMap<String, String>>
+)
--- src/main/kotlin/com/thoughtworks/sea/oidc/model/TokenResponse.kt
+++ src/main/kotlin/com/thoughtworks/sea/oidc/model/TokenResponse.kt
@@ -1,16 +1,6 @@
 package com.thoughtworks.sea.oidc.model
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import org.springframework.http.HttpEntity
-import org.springframework.util.LinkedMultiValueMap
-
-/** Those are params for getting token by using restTemplate. */
-data class TokenRequest(
-    /** Getting token url. */
-    val url: String,
-    /** HttpEntity contains request body and header. */
-    val httpEntity: HttpEntity<LinkedMultiValueMap<String, String>>
-)
 
 /** Those are token response from IDP. */
 data class TokenResponse(
