2b811be427ac209a16464e6a060a1c1d5175a8ac
yuexie.zhou <yuexie.zhou@thoughtworks.com>
2020-06-28 16:09:53 +0800

    [#130][Yuexie & Fuyi]: add encryption and decryption AESUtils for private key

---
+++ src/main/java/com/thoughtworks/provider/singpass/utils/AESUtils.java
@@ -0,0 +1,52 @@
+package com.thoughtworks.provider.singpass.utils;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Objects;
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import org.apache.commons.lang.StringUtils;
+
+public class AESUtils {
+
+  static final String IV = "tIo0D7UpXjpxsVvK";
+  static final int SECRET_KEY_LENGTH = 24; // 192 bit
+  static final String MODE = "AES/CBC/PKCS5PADDING";
+  static final String PAD_LETTER = "1";
+
+  public static String encrypt(String strToEncrypt, String secret) throws Exception {
+    IvParameterSpec iv = new IvParameterSpec(IV.getBytes(StandardCharsets.UTF_8));
+    SecretKeySpec skeySpec =
+        new SecretKeySpec(parseSecretKey(secret).getBytes(StandardCharsets.UTF_8), "AES");
+
+    Cipher cipher = Cipher.getInstance(MODE);
+    cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+
+    return Base64.getEncoder()
+        .encodeToString(cipher.doFinal(strToEncrypt.getBytes(StandardCharsets.UTF_8)));
+  }
+
+  public static String decrypt(String strToDecrypt, String secret) throws Exception {
+    IvParameterSpec iv = new IvParameterSpec(IV.getBytes(StandardCharsets.UTF_8));
+    SecretKeySpec skeySpec =
+        new SecretKeySpec(parseSecretKey(secret).getBytes(StandardCharsets.UTF_8), "AES");
+
+    Cipher cipher = Cipher.getInstance(MODE);
+    cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+
+    return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
+  }
+
+  private static String parseSecretKey(String key) throws Exception {
+    if (Objects.isNull(key)) {
+      throw new Exception("AESUtils: Secret key must not be empty");
+    }
+
+    if (key.length() == SECRET_KEY_LENGTH) return key;
+    if (key.length() < SECRET_KEY_LENGTH)
+      return StringUtils.rightPad(key, SECRET_KEY_LENGTH, PAD_LETTER);
+
+    return key.substring(0, SECRET_KEY_LENGTH);
+  }
+}
---
+++ src/test/java/com/thoughtworks/provider/singpass/utils/AESUtilsTest.java
@@ -0,0 +1,90 @@
+package com.thoughtworks.provider.singpass.utils;
+
+import static org.junit.Assert.assertEquals;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+@TestInstance(Lifecycle.PER_CLASS)
+class AESUtilsTest {
+  public static final String SECRET_KEY_16 = "tIo0D7UpXjpxsVvKo9o9";
+  public static final String SECRET_KEY_24 = "tIo0D7UpXjpxsVvKo9o91111";
+  public static final String SECRET_KEY_32 = "tIo0D7UpXjpxsVvKo9o9111111111111";
+
+  @Test
+  void should_success_when_encrypt_content() throws Exception {
+    // when
+    String encrypt = AESUtils.encrypt("1", SECRET_KEY_24);
+
+    assertEquals(encrypt, "grl8CVPruk/r3jrMPi92DQ==");
+  }
+
+  @Test
+  void should_success_when_decrypt_content() throws Exception {
+    // given
+    String encryptedText = "grl8CVPruk/r3jrMPi92DQ==";
+
+    // when
+    String decrypt = AESUtils.decrypt(encryptedText, SECRET_KEY_24);
+
+    // then
+    assertEquals(decrypt, "1");
+  }
+
+  @Test
+  void should_success_when_encrypt_and_decrypt() throws Exception {
+    // given
+    String content =
+        IOUtils.toString(this.getClass().getResourceAsStream("/keycloakPrivateKey.pem"), "UTF-8");
+
+    // when
+    String encryptText = AESUtils.encrypt(content, SECRET_KEY_24);
+    String decryptText = AESUtils.decrypt(encryptText, SECRET_KEY_24);
+
+    // then
+    assertEquals(content, decryptText);
+  }
+
+  @Test
+  void should_success_when_encrypt_and_decrypt_given_secret_key_length_less_than_24()
+      throws Exception {
+    // given
+    String content = "1";
+
+    // when
+    String encryptText = AESUtils.encrypt(content, SECRET_KEY_16);
+    String decryptText = AESUtils.decrypt(encryptText, SECRET_KEY_16);
+
+    // then
+    assertEquals(content, decryptText);
+  }
+
+  @Test
+  void should_success_when_encrypt_and_decrypt_given_secret_key_length_greater_24()
+      throws Exception {
+    // given
+    String content = "1";
+
+    // when
+    String encryptText = AESUtils.encrypt(content, SECRET_KEY_32);
+    String decryptText = AESUtils.decrypt(encryptText, SECRET_KEY_32);
+
+    // then
+    assertEquals(content, decryptText);
+  }
+
+  @Test
+  void should_throw_exception_when_secret_key_is_null() {
+    // given
+    String content = "1";
+
+    // when
+    Assertions.assertThrows(
+        Exception.class,
+        () -> AESUtils.encrypt(content, null),
+        "AESUtils: Secret key must not be empty");
+  }
+}
