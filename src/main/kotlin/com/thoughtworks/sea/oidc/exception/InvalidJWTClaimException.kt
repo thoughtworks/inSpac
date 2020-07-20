46c31a886cbae76f8ca58b9511c96045c9a692ac
Fu Yi <yifu@thoughtworks.com>
2020-07-20 14:44:30 +0800

    [#183][Yi & Tao]: add description for custom exception

--- src/main/kotlin/com/thoughtworks/sea/oidc/exception/InvalidJWTClaimException.kt
+++ src/main/kotlin/com/thoughtworks/sea/oidc/exception/InvalidJWTClaimException.kt
@@ -1,3 +1,6 @@
 package com.thoughtworks.sea.oidc.exception
 
+/**
+* Throw the exception with detail message when JWT Claim is invalid
+*/
 class InvalidJWTClaimException(message: String) : Throwable(message)
