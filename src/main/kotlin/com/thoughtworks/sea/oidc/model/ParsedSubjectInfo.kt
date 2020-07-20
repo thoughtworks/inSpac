8677b5fecad0db4d3ebca61b019d1a621d8562cb
Fu Yi <yifu@thoughtworks.com>
2020-07-20 17:38:47 +0800

    [#183][Yi]: add description for ParsedSubjectInfo

--- src/main/kotlin/com/thoughtworks/sea/oidc/model/ParsedSubjectInfo.kt
+++ src/main/kotlin/com/thoughtworks/sea/oidc/model/ParsedSubjectInfo.kt
@@ -1,3 +1,7 @@
 package com.thoughtworks.sea.oidc.model
 
+/**
+ * @description The subject information from OIDC response
+ * @param uuid: user's unique id
+ */
 data class ParsedSubjectInfo(val nricNumber: String?, val uuid: String)
