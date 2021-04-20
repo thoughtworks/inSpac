b093a052cf6914b5cb3fb5798388fdc747802b94
Hackl0us <hello@hackl0us.com>
2021-04-20 17:33:48 +0800

    feat(ci): add SonarQube gradle dependencies

--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -7,10 +7,10 @@ plugins {
     id("org.jetbrains.dokka") version "0.10.1"
     id("org.jmailen.kotlinter") version "2.3.2"
     id("org.owasp.dependencycheck") version "5.3.2.1"
-    id("org.sonarqube") version "3.0"
+    id("org.sonarqube") version "3.1.1"
 }
 
-apply{
+apply {
     from("gradle/jacoco.gradle")
 }
 
